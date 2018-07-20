bba2ef9c8ce7857c849d38a4ededd094d0117d93
Imirie <DImirie@smithbucklin.com>
2018-07-20 17:03:40 -0400

    added session capabilites, made html/css look better

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 import validation as v
 from network import Network
 
-network = Network()
+# network = Network()
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    network.ip, network.mask = [], []
+#    if not session.get("network"):
+#        session["network"] =  Network()
+    session["network"].ip, session["network"].mask = [], []
     if request.method == "POST":
-            network.ip = v.check_ip(request.form.get("ip"))
-            network.mask, network.cidr = v.check_mask(request.form.get("mask"))
-    return render_template("index.html", network=network)
+            session["network"].ip = v.check_ip(request.form.get("ip"))
+            session["network"].mask, session["network"].cidr = v.check_mask(request.form.get("mask"))
+    return render_template("index.html", network=session["network"])
